25b4e1257b2b0569f01faf0584465f305f2b247d
iwashito230 <iwashitotumire@gmail.com>
2019-04-25 19:36:34 +0900

    bottleで動作するようにした

---
+++ index.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+from bottle import route, run
+from bottle import jinja2_template as template
+from bottle import static_file
+
+
+@route('/css/<filename>')
+def css_dir(filename):
+    return static_file(filename, root='./static/css')
+
+
+@route('/css/nivo-lightbox/<filename>')
+def css_nivo_dir(filename):
+    return static_file(filename, root='./static/css/nivo-lightbox')
+
+
+@route('/js/<filename>')
+def js_dir(filename):
+    return static_file(filename, root='./static/js')
+
+
+@route('/img/<filename>')
+def img_dir(filename):
+    return static_file(filename, root='./static/img')
+
+
+@route('/fonts/<filename>')
+def fonr_dir(filename):
+    return static_file(filename, root='./static/fonts')
+
+
+@route('/')
+def top():
+    return template('index')
+
+
+if __name__ == '__main__':
+    run(host='localhost', port=8080, debug=True, reloader=True)
